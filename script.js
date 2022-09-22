de4d905bcaeb309c69c7f08a2346f14b171b9d1e
rajatmohan <rajatm2196@gmail.com>
2022-09-22 23:40:45 +0530

    Change grid size according to user input

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ const createGridCell = (rowNo, columnNo)=> {
     divEle.style.gridColumn = columnNo;
 
     divEle.addEventListener("mouseenter",divMouseEnter);
-    
+
     return divEle;
 }
 
 const createGrid = (numberRows, numberColumns)=> {
     const gridContainer = document.querySelector(".grid-container");
+    // Remove all childrens of grid container by setting textContent to empty string
+    gridContainer.textContent = '';
     for(let rowNo = 1; rowNo <= numberRows; rowNo++) {
         for(let columnNo = 1; columnNo <= numberColumns; columnNo++) {
             gridContainer.appendChild(createGridCell(rowNo, columnNo));
@@ -24,7 +26,21 @@ const createGrid = (numberRows, numberColumns)=> {
     }
 }
 
-createGrid(32, 32);
+const gridSizeChooseBtn = document.querySelector("#gridSize")
+gridSizeChooseBtn.addEventListener("click", (event)=> {
+    let numberCellsPerSide = parseInt(prompt("Enter number of squares per side beteen 1 and 100."));
+    if(isNaN(numberCellsPerSide) || numberCellsPerSide <= 0 || numberCellsPerSide > 100) {
+        alert("Wrong Input");
+    }
+    else {
+        createGrid(numberCellsPerSide, numberCellsPerSide);
+    }
+})
+
+createGrid(16, 16);
+
+
+
 
 
 
