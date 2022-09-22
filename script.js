c6018703d8f025dbe44e4f0ab2e02643b2480070
rajatmohan <rajatm2196@gmail.com>
2022-09-22 23:57:55 +0530

    Add random color to div on mouse enter

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+// generateRandomNo will generate random no between 0 (including) and n (excluding)
+const generateRandomNo = (n) => {
+    return Math.floor(Math.random()*n);
+}
+
+const generateRandomColor = () => {
+    // Color maximum value in hexadecimal would be 0xFFFFFF
+    // One Number greater than maximum color would be 0x1000000
+    // toString(16) convert number into hexadecimal string representation 
+    return '#'+generateRandomNo(0x1000000).toString(16);;
+}
+
 const divMouseEnter = (event)=> {
     const divEle = event.target;
-    divEle.style.backgroundColor = 'black';
+    divEle.classList.add("visited");
+    divEle.style.backgroundColor = generateRandomColor();
 }
 
 const createGridCell = (rowNo, columnNo)=> {
