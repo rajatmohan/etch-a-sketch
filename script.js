a3deaee42775144ceedfc69703888192290f3e12
rajatmohan <rajatm2196@gmail.com>
2022-09-22 23:24:44 +0530

    Change color of divon mouse enter

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const gridContainer = document.querySelector(".grid-container")
+const divMouseEnter = (event)=> {
+    const divEle = event.target;
+    divEle.style.backgroundColor = 'black';
+}
 
 const createGridCell = (rowNo, columnNo)=> {
     divEle = document.createElement('div');
     divEle.classList.add("grid-item");
+    
     divEle.style.gridRow = rowNo;
     divEle.style.gridColumn = columnNo;
+
+    divEle.addEventListener("mouseenter",divMouseEnter);
+    
     return divEle;
 }
 
 const createGrid = (numberRows, numberColumns)=> {
+    const gridContainer = document.querySelector(".grid-container");
     for(let rowNo = 1; rowNo <= numberRows; rowNo++) {
         for(let columnNo = 1; columnNo <= numberColumns; columnNo++) {
             gridContainer.appendChild(createGridCell(rowNo, columnNo));
